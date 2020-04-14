da61a029dfabfb163f45aa6527091f0deb82b225
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-04-13 20:25:35 -0400

    Set up initial socket.io boilerplate

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express"),
+    app = express(),
+    port = process.env.PORT || 5000;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+let clients = {};
+
+io.on("connect", (socket) => {
+    const token = socket.handshake.query.token;
+
+    if (clients[token] == null) {
+        clients[token] = [socket];
+    } else clients[token].push(socket);
+
+    console.log(socket.handshake.query.token);
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected!");
+    });
+
+    socket.on("event", (eventData) => {
+        console.log(eventData);
+    });
+
+    console.log("a user connected!");
+});
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
