d84fe0de11c727e5f85d89605229c90894f4c3eb
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-04-13 20:37:59 -0400

    Added cors middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express"),
     app = express(),
-    port = process.env.PORT || 5000;
+    port = process.env.PORT || 5001;
+const cors = require("cors");
+app.use(cors);
+
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
